a544779f62e9d6b1c326cb27f019bb4c663683a6
kevinkaos <kevinswork126@gmail.com>
2024-11-30 00:23:35 +0800

    completed blog-preview-card challenge

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
 
 export default {
   content: [
@@ -11,8 +12,31 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        yellow: "hsl(47deg, 88%, 63%)",
+        "gray-950": "hsl(0deg, 0%, 7%)",
+        "gray-500": "hsl(0deg, 0%, 42%)",
       },
+      boxShadow: {
+        'dropShadow': '8px 8px 0px 0px black',
+      }
     },
+    spacing: {
+      300: "24px",
+      150: "12px",
+      100: "8px",
+      50: "4px",
+    },
+    fontFamily: {
+      sans: ["var(--font-figtree)"],
+    }
   },
-  plugins: [],
+  plugins: [
+    plugin(function({ addComponents, theme }) {
+      addComponents({
+        ".text-preset-1": {
+          fontSize: "200px",
+        },
+      }); 
+    })
+  ],
 } satisfies Config;
